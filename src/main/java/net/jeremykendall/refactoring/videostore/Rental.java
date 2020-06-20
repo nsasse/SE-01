af7564cdd742712c62778ff02f85f597c94d6640
Tim <43130816+DerStimmler@users.noreply.github.com>
2020-06-20 14:02:29 +0200

    Anforderung 2: Moved Methods

--- src/main/java/net/jeremykendall/refactoring/videostore/Rental.java
+++ src/main/java/net/jeremykendall/refactoring/videostore/Rental.java
@@ -17,35 +17,12 @@ public class Rental {
         return _daysRented;
     }
 
-    public double amountFor() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented() - 2) *
-                            1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented() - 3) *
-                            1.5;
-                break;
-        }
-        return result;
-    }
-
     public int getFrequentRenterPoints() {
 
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                getDaysRented() > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return _movie.getFrequentRenterPoints(_daysRented);
+    }
+
+    public double amountFor() {
+        return _movie.amountFor(_daysRented);
     }
 }
