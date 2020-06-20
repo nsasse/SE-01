b3a4ad45a152275a581c439e3adb85dc4f7e9f43
Mathiskamue <44195325+Mathiskamue@users.noreply.github.com>
2020-06-20 13:22:59 +0200

    2. Anforderung: Moving Method

--- src/main/java/net/jeremykendall/refactoring/videostore/Customer.java
+++ src/main/java/net/jeremykendall/refactoring/videostore/Customer.java
@@ -20,8 +20,9 @@ public class Customer {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
 
+
             // determine amount for each line
-            thisAmount = amountFor(each);
+            thisAmount = each.amountFor();
 
             // add frequent renter points
             frequentRenterPoints ++;
@@ -49,25 +50,5 @@ public class Customer {
         return _name;
     }
 
-    private double amountFor(Rental each){
-        double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) *
-                            1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) *
-                            1.5;
-                break;
-        }
-        return thisAmount;
-    }
+
 }
